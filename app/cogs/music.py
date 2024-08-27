0f453a9ab192e91d586114eeab93f5ade489c3d8
Rupert Jelnikar Mrak <jelnikar.mrak.rupert@gmail.com>
2024-08-27 02:00:40 +0200

    Fixed the queue emptiying because of a get_at in the create_mp_embeds replaced it with peek

--- app/cogs/music.py
+++ app/cogs/music.py
@@ -295,7 +295,7 @@ class MusicCog(commands.GroupCog, name='music'):
         queue_content = ''
         if not player.queue.is_empty == True:
             for i in range(player.queue.count - 1, -1, -1):
-                track = player.queue.get_at(i)
+                track = player.queue.peek(i)
                 queue_content += f'{i + 1}. [{track.title}]({track.uri})\n'
         else:
             queue_content = 'No audio in the queue.'
